1a50ba3f334ea28779ca4a36da3f7ad6d5e3cb7d
aqib gul <aqibgul880@gmail.com>
2025-07-04 23:01:50 +0500

    find gender & probability of names

--- app/page.js
+++ app/page.js
@@ -1,103 +1,93 @@
-import Image from "next/image";
+"use client";
+import { Star, User } from "lucide-react";
+import { use, useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export default function Home() {
+const Home = () => {
+  const [userData, setUserData] = useState({});
+  const [inputValue, setInputValue] = useState("");
+  const [gender, setGender] = useState("");
+  const [probability, setProbability] = useState("");
+  const router = useRouter();
+
+  const fetchData = async () => {
+    const res = await fetch(`https://api.genderize.io/?name=${inputValue}`);
+    const data = await res.json();
+    console.log("Fetched Data:", data);
+    setUserData(data);
+    router.push(`/?name=${inputValue}`);
+    setGender(data.gender || "Gender");
+    setProbability(data.probability || "0");
+  };
+  const handleSubmit = () => {
+    if (inputValue.trim() === "") {
+      alert("Please enter a name");
+      return;
+    }
+    fetchData();
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="h-screen w-full bg-blue-200 flex flex-col items-center justify-center ">
+        <h1 className="text-4xl font-sevillana font-bold ">Welcome</h1>
+        <div
+          className=" flex flex-col items-center 
+        gap-3 w-[20%] h-[50%] bg-red-200  rounded-2xl p-3 "
+        >
+          <User className="rounded-full bg-pink-400" size={58} />
+          <p className="text-3xl font-bold font-sevillana ">
+            {inputValue || " Name"}
+          </p>
+          <p className="text-2x1 rounded-full bg-pink-300 p-2">
+            {gender || "Gender"}
+          </p>
+          <div className="space-y-15  ">
+            <div className=" w-3xs  bg-gray-200 rounded-lg p-4">
+              <div className="flex items-center justify-between mb-2">
+                <span className="text-sm font-medium text-black-500 flex items-center">
+                  <Star className="size-4 " />
+                  Confidence
+                </span>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+                <span className="text-sm font-medium text-gray-800">
+                  {`${(probability * 100).toFixed(2)}%`}
+                </span>
+              </div>
+              <div className="w-full h-3 rounded-full bg-pink-200">
+                <div
+                  className="h-full bg-pink-500 rounded-full"
+                  style={{
+                    width: `${(probability * 100).toFixed(2)}%`,
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-gray-200 font-bold rounded-full p-1.5">
+            <input
+              className=" bg-transparent outline-none text-center text-lg font-sevillana"
+              type="text"
+              placeholder="Enter your name"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          </div>
+          <button
+            onClick={handleSubmit}
+            className=" bg-black text-white font-bold rounded-full
+           p-2 hover:bg-gray-700 "
           >
-            Read our docs
-          </a>
+            click me
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
-}
+};
+export default Home;
