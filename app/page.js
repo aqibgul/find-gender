ef3f5fcd8b2751bf36686b9982b95f2e019accaa
aqib gul <aqibgul880@gmail.com>
2025-07-05 22:52:24 +0500

    bg changes

--- app/page.js
+++ app/page.js
@@ -30,24 +30,36 @@ const Home = () => {
   useEffect(() => {
     fetchData();
   }, []);
+  const isMale = userData.gender == "male";
 
   return (
     <>
-      <div className="h-screen w-full bg-blue-200 flex flex-col items-center justify-center ">
+      <div className="h-screen w-full  flex flex-col items-center justify-center ">
         <h1 className="text-4xl font-sevillana font-bold ">Welcome</h1>
         <div
-          className=" flex flex-col items-center 
-        gap-3 w-[20%] h-[50%] bg-red-200  rounded-2xl p-3 "
+          className={`flex flex-col items-center 
+        gap-3 w-[20%] h-[50%] shadow-md   rounded-2xl p-3 ${
+          isMale ? "bg-blue-200" : "bg-pink-200"
+        }`}
         >
-          <User className="rounded-full bg-pink-400" size={58} />
+          <User
+            className={`rounded-full  ${
+              isMale ? "bg-blue-500" : "bg-pink-500"
+            }`}
+            size={58}
+          />
           <p className="text-3xl font-bold font-sevillana ">
             {inputValue || " Name"}
           </p>
-          <p className="text-2x1 rounded-full bg-pink-300 p-2">
+          <p
+            className={`text-2x1 rounded-full  p-2 ${
+              isMale ? "bg-blue-400" : "bg-pink-400"
+            } `}
+          >
             {gender || "Gender"}
           </p>
           <div className="space-y-15  ">
-            <div className=" w-3xs  bg-gray-200 rounded-lg p-4">
+            <div className=" xl:w-3xs  bg-gray-200 rounded-lg p-4">
               <div className="flex items-center justify-between mb-2">
                 <span className="text-sm font-medium text-black-500 flex items-center">
                   <Star className="size-4 " />
@@ -58,9 +70,15 @@ const Home = () => {
                   {`${(probability * 100).toFixed(2)}%`}
                 </span>
               </div>
-              <div className="w-full h-3 rounded-full bg-pink-200">
+              <div
+                className={`w-full h-3 rounded-full ${
+                  isMale ? "bg-blue-200" : "bg-pink-200"
+                }`}
+              >
                 <div
-                  className="h-full bg-pink-500 rounded-full"
+                  className={`h-full  rounded-full ${
+                    isMale ? "bg-blue-500" : "bg-pink-500"
+                  }`}
                   style={{
                     width: `${(probability * 100).toFixed(2)}%`,
                   }}
@@ -69,7 +87,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="bg-gray-200 font-bold rounded-full p-1.5">
+          <div className="  bg-gray-200 font-bold rounded-full p-1.5">
             <input
               className=" bg-transparent outline-none text-center text-lg font-sevillana"
               type="text"
@@ -80,8 +98,10 @@ const Home = () => {
           </div>
           <button
             onClick={handleSubmit}
-            className=" bg-black text-white font-bold rounded-full
-           p-2 hover:bg-gray-700 "
+            className={`${
+              isMale ? "bg-blue-800" : "bg-pink-700"
+            } text-white font-bold rounded-full
+           p-2 hover:bg-gray-700`}
           >
             click me
           </button>
